f8bb65d7dc925df92e7c3cdb9d0f750e0da0d1a2
Khushal <khushal2004a@gmail.com>
2025-09-30 05:52:16 +0530

    feat: init

---
+++ App.tsx
@@ -0,0 +1,52 @@
+import {
+  ViroARScene,
+  ViroARSceneNavigator,
+  ViroVideo,
+  ViroTrackingReason,
+  ViroTrackingStateConstants,
+} from "@reactvision/react-viro";
+import React, { useState } from "react";
+import { StyleSheet } from "react-native";
+
+const HelloWorldSceneAR = () => {
+  const [visible, setVisible] = useState(false);
+
+  function onInitialized(state: any, reason: ViroTrackingReason) {
+    console.log("onInitialized", state, reason);
+    if (state === ViroTrackingStateConstants.TRACKING_NORMAL) {
+      setVisible(true);
+    }
+  }
+
+  return (
+    <ViroARScene onTrackingUpdated={onInitialized}>
+      {visible && (
+        <ViroVideo
+          source={require("./assets/sabrina.mp4")} // put sabrina.mp4 inside your project’s ./assets folder
+          position={[0, -1, -4]} // 3 meters in front of the camera
+          width={2} // 2 meters wide
+          height={1.2} // keep aspect ratio
+          loop={true} // loop video
+          muted={false} // play with sound
+          transformBehaviors={["billboardY"]}
+        />
+      )}
+    </ViroARScene>
+  );
+};
+
+export default () => {
+  return (
+    <ViroARSceneNavigator
+      autofocus={true}
+      initialScene={{
+        scene: HelloWorldSceneAR,
+      }}
+      style={styles.f1}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  f1: { flex: 1 },
+});
